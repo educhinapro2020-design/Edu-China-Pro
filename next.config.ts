2dd9e61156571f4337a56f62c96dd4c4521ca974
Nikesh Kazi Lepcha <nikeshlepcha7@gmail.com>
2026-03-08 16:10:39 +0545

    feat: openNext config files;

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
+  // ignores used because Next 15 uses webpack for builds; Remove them when moving to 16
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
